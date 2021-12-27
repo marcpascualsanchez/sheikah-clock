fbd388df51d2d20f150c23a1b4138bd2bfcddc81
marcpascualsanchez <marcpascualsanchez@gmail.com>
2021-12-27 20:23:18 +0100

    Fix leds

--- docs/readme.md
+++ docs/readme.md
@@ -1,11 +1,28 @@
 # Sheikah Clock
 ## Abstract
-Arduino based clock + weather display, with rgb leds. Retrieves data from (openweathermap)[https://openweathermap.org/] over HTTP using an ESP WiFi module.
+Arduino based clock + weather display, with rgb leds. Retrieves data from [openweathermap](https://openweathermap.org/) over HTTP using an ESP WiFi module.
 
 ## Details
-There are (9 different main weathers)[https://openweathermap.org/weather-conditions]. Each one is represented by an icon, which code is received by the (openweathermap)[https://openweathermap.org/] API.
+There are [9 different main weathers](https://openweathermap.org/weather-conditions). Each one is represented by an icon, which code is received by the [openweathermap](https://openweathermap.org/) API.
 
-In the clock, each icon is represented by the blue-lighted weather emblem. There are 7 positions, following the (sheikah plate design)[blob:https://web.telegram.org/e5807c23-0650-42d6-b40b-7d963ef8d7bd]. Each of the points represent one position. The position map to the (9 different weathers)[https://openweathermap.org/weather-conditions] are represented in the plain-schema in `./docs` (no Sheikah map points is mapped to (01)clear sky).
+In the clock, each icon is represented by the blue-lighted weather emblem. There are 7 positions, following the [sheikah plate design](blob:https://web.telegram.org/e5807c23-0650-42d6-b40b-7d963ef8d7bd). Each of the points represent one position. The position map to the [9 different weathers](https://openweathermap.org/weather-conditions) are represented in the plain-schema in `./docs` (no Sheikah map points is mapped to (01)clear sky).
 
 ## Configure the clock
-The device has three buttons. One to either start and end the time configuration (blue), and the other two to add (top black) and substract (bottom black) minutes or hours.
+The device has three buttons. One to either start and end the time configuration (blue), and the other two to add and substract minutes and hours. To accept, press again the configuration button (blue).
+
+### Technical details
+#### LED strip
+From 0 to 20, leds that will turn blue for each weather. Only one cut will be needed, from the SNOW weather (last led is 15) to RAIN2.
+
+WEATHER | LED light id*
+--- | ---
+CLEAR |                []
+MIST |                 [0, 1, 2]
+CLOUDS (1) |           [3]
+CLOUDS (2&3) |         [4, 5, 6, 7, 8]
+RAIN (1) |             [9, 10, 11, 12, 13, 18]
+RAIN (3) |             [13, 14, 15, 16]
+RAIN (2) |             [17]
+SNOW |                 [19, 20]
+
+*(by order from the power source until the end of the strip)
--- src/RGBLeds/RGBLeds.h
+++ src/RGBLeds/RGBLeds.h
@@ -8,21 +8,21 @@
 // !!! WARNING: RGB is not the right order -> GRB
 #define COLOR_ORDER GRB
 #define RGBLED_PIN 15
-#define NUM_LEDS 19
+#define NUM_LEDS 25
 #define LED_TYPE WS2812
 #define MIN_BRIGHTNESS 32
 #define MAX_BRIGHTNESS 128
 
 // enum weather led ranges
 const int CLEAR_SKY_LENGTH = 0;
-const int FEW_CLOUDS_LENGTH = 4;
-const int SCATTERED_CLOUDS_LENGTH = 7;
-const int BROKEN_CLOUDS_LENGTH = 7;
-const int MIST_LENGTH = 10;
-const int SHOWER_RAIN_LENGTH = 3;
-const int RAIN_LENGTH = 6;
-const int THUNDERSTORM_LENGTH = 8;
-const int SNOW_LENGTH = 10;
+const int FEW_CLOUDS_LENGTH = 1;
+const int SCATTERED_CLOUDS_LENGTH = 6;
+const int BROKEN_CLOUDS_LENGTH = 6;
+const int MIST_LENGTH = 9;
+const int SHOWER_RAIN_LENGTH = 11;
+const int RAIN_LENGTH = 15;
+const int THUNDERSTORM_LENGTH = 18;
+const int SNOW_LENGTH = 15;
 
 class RGBLeds
 {
@@ -40,15 +40,15 @@ public:
     // uint8_t inactiveColor[3] = {85, 176, 0}; // orange
     uint8_t activeColor[3] = {14, 54, 150}; // dark blue
     uint8_t inactiveColor[3] = {220, 85, 0}; // orange
-    int CLEAR_SKY_RANGE[CLEAR_SKY_LENGTH] = {};                                    // clear sky
-    int FEW_CLOUDS_RANGE[FEW_CLOUDS_LENGTH] = {9, 8, 7, 3};                        // few clouds
-    int SCATTERED_CLOUDS_RANGE[SCATTERED_CLOUDS_LENGTH] = {9, 8, 7, 6, 5, 4, 3};   // scattered clouds
-    int BROKEN_CLOUDS_RANGE[BROKEN_CLOUDS_LENGTH] = {9, 8, 7, 6, 5, 4, 3};         // broken clouds
-    int MIST_RANGE[MIST_LENGTH] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};                  // mist
-    int SHOWER_RAIN_RANGE[SHOWER_RAIN_LENGTH] = {9, 10, 11};                       // shower rain
-    int RAIN_RANGE[RAIN_LENGTH] = {9, 10, 11, 14, 15, 16};                         // rain
-    int THUNDERSTORM_RANGE[THUNDERSTORM_LENGTH] = {9, 10, 11, 14, 15, 16, 17, 18}; // thunderstorm
-    int SNOW_RANGE[SNOW_LENGTH] = {9, 10, 11, 12, 13, 14, 15, 16, 17, 18};         // snow
+    int CLEAR_SKY_RANGE[CLEAR_SKY_LENGTH] = {};                                                                         // 01 clear sky
+    int FEW_CLOUDS_RANGE[FEW_CLOUDS_LENGTH] = {3};                                                                      // 02 few clouds
+    int SCATTERED_CLOUDS_RANGE[SCATTERED_CLOUDS_LENGTH] = {3, 4, 5, 6, 7, 8};                                           // 03 scattered clouds
+    int BROKEN_CLOUDS_RANGE[BROKEN_CLOUDS_LENGTH] = {3, 4, 5, 6, 7, 8};                                                 // 04 broken clouds
+    int MIST_RANGE[MIST_LENGTH] = {0, 1, 2, 3, 4, 5, 6, 7, 8};                                                          // 50 mist
+    int SHOWER_RAIN_RANGE[SHOWER_RAIN_LENGTH] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20};                                  // 09 shower rain
+    int RAIN_RANGE[RAIN_LENGTH] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 18, 19};                                // 10 rain
+    int THUNDERSTORM_RANGE[THUNDERSTORM_LENGTH] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 20, 15, 16, 17, 18, 19};    // 11 thunderstorm
+    int SNOW_RANGE[SNOW_LENGTH] = {3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 20, 21, 22, 23, 24};                                // 13 snow
 };
 
 #endif
